fba65a428110888ae892c8317a07f7a5c3dfa8f8
Bernardo <bernardo_limas@hotmail.com>
2023-06-23 19:23:34 -0300

    bugfix: change bgColor from menu and height from header

--- src/components/sections/Header.tsx
+++ src/components/sections/Header.tsx
@@ -5,7 +5,7 @@ export function Header() {
   const [openMenu, setOpenMenu] = useState(false)
 
   return (
-    <div className="w-full h-[120px] bg-white flex items-center justify-center border-solid border-b fixed border-separatorHeader z-50">
+    <div className="w-full h-[90px] md:[120px]] bg-white flex items-center justify-center border-solid border-b fixed border-separatorHeader z-50">
       <div className="w-full max-w-[1440px] mx-auto px-8">
         <div className="w-full flex items-center justify-between">
           <div className="md:hidden"></div>
@@ -29,19 +29,19 @@ export function Header() {
             <div className="md:collapse">
               <button type="button" onClick={() => setOpenMenu(!openMenu)} className=""><List size={34}/></button>
           {openMenu && (
-              <nav className='absolute right-0 top-[7.5rem] w-[50vw] h-[820px] bg-body opacity-70 flex flex-col justify-center'>
+              <nav className='absolute right-0 top-[5.625rem] w-[50vw] h-[880px] bg-body opacity-90 flex flex-col justify-center'>
                 <ul className='flex flex-col items-center justify-center gap-2'>
                   <li 
                     className='w-full h-12 border-b border-t border-solid border-separatorHeader text-white font-medium text-lg flex items-center justify-center hover:text-purple500'>
-                      <a href="">Home</a>
+                      <a href="#home">Home</a>
                   </li>
                   <li 
                     className='w-full h-12 border-b border-solid border-separatorHeader text-white font-medium text-lg flex items-center justify-center hover:text-purple500'>
-                      <a href="">Projetos</a>
+                      <a href="#projetos">Projetos</a>
                   </li>
                   <li 
                     className='w-full h-12 border-b border-solid border-separatorHeader text-white font-medium text-lg flex items-center justify-center hover:text-purple500'>
-                      <a href="">Contatos</a>
+                      <a href="#contatos">Contatos</a>
                   </li>
                 </ul>
               </nav>
